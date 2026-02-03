7a2e2ca8b1e05e809052007d30590ea3358d7cb4
martinLivGit <fligm1215@gmail.com>
2026-02-03 20:43:14 +0000

    Update AccountAssignment.scala

--- src/main/scala/com/quantexa/assignments/accounts/AccountAssignment.scala
+++ src/main/scala/com/quantexa/assignments/accounts/AccountAssignment.scala
@@ -85,8 +85,7 @@ object AccountAssignment {
       val acctLst = acctIt.toList
       val (accounts, totalBalance) = acctLst.foldLeft((List[AccountData](),0L)) {
          case ( (l: List[AccountData], t: Long), ca: (String,(CustomerData,AccountData))) => {
-            if ( ca._2._2 != null ) (l :+ ca._2._2, t + ca._2._2.balance)
-            else ( l, t)
+            if ( ca._2._2 != null ) (l :+ ca._2._2, t + ca._2._2.balance) else ( l, t)
          }
       }
       val (forename, surname) = if (acctLst.head._2._1 != null) (acctLst.head._2._1.forename, acctLst.head._2._1.surname) else ("","")
