4870b68bb99a3596281df2d7ffaccc916a3fb7fa
martinLivGit <fligm1215@gmail.com>
2026-02-08 18:01:06 +0000

    Update AccountAssignment.scala

--- src/main/scala/com/quantexa/assignments/accounts/AccountAssignment.scala
+++ src/main/scala/com/quantexa/assignments/accounts/AccountAssignment.scala
@@ -85,7 +85,7 @@ object AccountAssignment {
       val acctLst = acctIt.toList
       val (accounts, totalBalance) = acctLst.foldLeft((List[AccountData](),0L)) {
          case ((l:List[AccountData],t:Long),(_,(_,account:AccountData))) if account != null => (l :+ account, t + account.balance)
-         case ((l:List[AccountData],t:Long),_) if account != null => (l,t)       
+         case ((l:List[AccountData],t:Long),_) => (l,t)       
       }
       val avgBalance = if ( accounts.isEmpty) 0 else totalBalance/accounts.size
       val (forename, surname) = if (acctLst.head._2._1 != null) (acctLst.head._2._1.forename, acctLst.head._2._1.surname) else ("","")
