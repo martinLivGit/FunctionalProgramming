fe24fc3749c6ac379b0e0fac96e9a024da25d68b
martinLivGit <fligm1215@gmail.com>
2026-02-24 19:02:32 +0000

    Update TransactionAssignmentSolutionSuite.scala
    test for the account/dates without records

--- src/test/scala/com/quantexa/assignments/transactions/TransactionAssignmentSolutionSuite.scala
+++ src/test/scala/com/quantexa/assignments/transactions/TransactionAssignmentSolutionSuite.scala
@@ -1,20 +1,61 @@
 package com.quantexa.assignments.transactions
 
 //import org.scalatest.FunSuite
-import org.scalatest.fixture
+import com.quantexa.assignments.transactions.TransactionAssignment.txnAggregatorWithFold
+import org.scalatest.{FunSuite, fixture}
 
-class TransactionAssignmentSolutionSuite extends TransactionAssignmentBaseSuite {
+import scala.io.Source
 
-   val accountStats: List[DayAccountStats] = TransactionAssignment(transactions)
+class TransactionAssignmentSolutionSuite extends FunSuite {
 
-   def withFixture(test: OneArgTest) = {
+  //The full path to the file to import
+  val fileName : String = "C:/Users/martin/Documents/Dev/Project/Quantexa/Spark Project Template/SparkTestProjectWithAnswers/SparkTestProject/build/resources/test/transactions.csv";
 
-      // create the fixture
-      val theFixture = FixtureParam(accountStats)
+  //The lines of the CSV file (dropping the first to remove the header)
+  val transactions : Iterator[String]  = Source.fromFile(fileName).getLines().drop(1)
 
-      try {
-         withFixture(test.toNoArgTest(theFixture)) // "loan" the fixture to the test
-      }
-   }
+  val transactionsList : List[Transaction] = transactions.map {
+    line =>
+      val split = line.split(',')
+      Transaction(split(0), split(1), split(2).toInt, split(3), split(4).toDouble)
+  }.toList
+
+  val dayAccountStats: List[DayAccountStats] = TransactionAssignment(transactionsList,txnAggregatorWithFold)
+
+  test("dayAccountStats print") {
+    println(dayAccountStats.sortBy(p=>(p.accountId,p.transactionDay)))
+    assert( true )
+  }
+
+  test("dayAccountStats result checks") {
+    assert( dayAccountStats.contains(DayAccountStats(4,"A11",914.89,597.085,1906.46,0.0,0.0)) )
+    assert( dayAccountStats.contains(DayAccountStats(24,"A18",965.08,470.02000000000004,554.78,932.72,183.53)))
+  }
+
+  test("accounts stats") {
+    assert( dayAccountStats.map(x=>x.accountId).toSet == Set("A9", "A22", "A47", "A33", "A11", "A4", "A10", "A15", "A32", "A26", "A21", "A37", "A46", "A41", "A12", "A5", "A16", "A49", "A38", "A27", "A31", "A34", "A23", "A19", "A20", "A39", "A45", "A17", "A28", "A2", "A13", "A40", "A42", "A1", "A6", "A30", "A35", "A24", "A36", "A29", "A43", "A18", "A8", "A3", "A14", "A44", "A7", "A25", "A48"))
+    assert( dayAccountStats.size == 1369)
+    assert( dayAccountStats.map(x=>x.accountId).toSet.size == 49 )
+  }
+
+  test("missing records account for") {
+
+    val accounts = dayAccountStats.map(x=>x.accountId).toSet
+    val mapDayAccountStats = (
+      for {
+        day <- 1 to 31
+        account <- accounts
+        stat <- dayAccountStats if stat.transactionDay == day && stat.accountId == account
+      } yield (account, day ) -> stat
+    ).toMap
+
+    val completeDayAccountStats = ( for {
+      day <- 1 to 31
+      account <- accounts
+    } yield mapDayAccountStats.getOrElse((account,day),DayAccountStats(day,account,-1D,-1D,-1D,-1D,-1D))).toList
+
+    assert(dayAccountStats.map(x=>x.accountId).toSet.size * 31 == completeDayAccountStats.size)
+    
+  }
 
 }
